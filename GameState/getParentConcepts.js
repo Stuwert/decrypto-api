66dff468991adc9f09dade6e0a3c8fe588b6cfc0
Stuart Urback <stuart@urback.net>
2019-05-04 00:11:44 -0600

    Add parent concept query

---
+++ GameState/getParentConcepts.js
@@ -0,0 +1,37 @@
+const knex = require('../db/knex');
+
+
+const getRootQuery = (gameId) => knex
+  .from('parent_concepts')
+  .join('game_answers', 'parent_concepts.id', 'game_answers.parent_concept_id')
+  .where('game_answers.game_id', gameId);
+
+const getParentConcepts = async (gameId) => {
+  const parentConcepts = await getRootQuery(gameId)
+    .select('parent_concepts.id')
+
+  return parentConcepts.map(({ id }) => id);
+}
+
+const formatAnswers = ({
+  id,
+  name,
+}) => ({
+  parentConceptId: id,
+  parentConcept: name,
+});
+
+const getGameAnswers = async (gameId) => {
+  const gameAnswers = await getRootQuery(gameId)
+    .select(
+      'parent_concpets.id',
+      'parnet_concepts.name'
+    );
+
+  return formateAnswers(gameAnswers);
+}
+
+module.exports = {
+  getParentConcepts,
+  getGameAnswers
+};
