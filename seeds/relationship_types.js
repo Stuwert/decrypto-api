753c762e5266740dca140cde70503c6fd1e36f7f
Stuart Urback <stuart@urback.net>
2019-05-06 20:45:57 -0600

    Clean up seeds more

--- seeds/relationship_types.js
+++ seeds/relationship_types.js
@@ -1,10 +1,10 @@
 
 exports.seed = function (knex, Promise) {
   // Deletes ALL existing entries
-  return knex('parent_child_relationships').del()
+  return knex('parent_child_concept_relationships').del()
     .then(function () {
       // Inserts seed entries
-      return knex('parent_child_relationships').insert([
+      return knex('parent_child_concept_relationships').insert([
         { id: 1, colName: 'synonyms' },
         { id: 2, colName: 'typeOf' },
         { id: 3, colName: 'examples' },
